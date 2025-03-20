855c9d81accc7c9197b7bbe6d8b0b6c4fb932ab1
Amalp710 <124747158+Amalp710@users.noreply.github.com>
2025-03-20 17:31:31 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <BrowserRouter  basename="/netflix-clone/">
+    <BrowserRouter  basename="/netflix-clone-1/">
         <App />
 
     </BrowserRouter>
